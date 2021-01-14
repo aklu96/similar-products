b244cc770724b3df6184dcf104a05a66e20de0a8
Alex Klyuev <aklyuev96@gmail.com>
2021-01-14 16:28:20 -0500

    Add webpack config file and setup barebones React app

---
+++ client/src/components/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => (
+  <div>React App is setup</div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, 'client', 'src');
+const OUT_DIR = path.join(__dirname, 'public');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    filename: 'bundle.js',
+    path: OUT_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  mode: 'development'
+}
