4c34e9d5058bc97de7346455d15b9bf6d863bc5c
Alex Klyuev <aklyuev96@gmail.com>
2021-01-15 22:29:03 -0500

    refactor database to be more readable implement get request for product by id

--- database/seed.js
+++ database/seed.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
+const dbConnect = require('./dbConnect.js');
 const Product = require('./Product.js');
-const products = require('./data.js');
+const products = require('./data/data.js');
 
 // set up database
-mongoose.connect('mongodb://localhost/fjords', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost/fjords', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// create and connect to database
+dbConnect();
 
 Product.create(products)
   .then(() => mongoose.disconnect())
