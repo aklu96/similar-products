1e3e251e7bea907b3177f8328244721283b11369
Alex Klyuev <aklyuev96@gmail.com>
2021-03-28 17:22:42 -0700

    add compression

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-console */
 const express = require('express');
-// const morgan = require('morgan');
 const cors = require('cors');
+const compression = require('compression');
 const dbConnect = require('../database/dbConnect.js');
 const controller = require('./controller.js');
 
 // server setup
 const PORT = process.env.PORT || 3002;
 const app = express();
-// app.use(morgan('dev'));
 app.use(cors());
+app.use(compression());
 app.use(express.json());
 app.use(express.static(`${__dirname}/../public`));
 
